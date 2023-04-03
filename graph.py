fe73c2d28a00052bac2156431d273f7b6317c3a9
Luis Gabriel López Soler <lopezsolerluis@gmail.com>
2023-04-03 11:40:06 -0300

    Process a bunch of measures taken in a row

--- graph.py
+++ graph.py
@@ -2,50 +2,57 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import os
 
 file = sys.argv[1]
 logp = sys.argv[2] if len(sys.argv)>2 else 'lin'
 
-def process_df(df, log):
-    print(df)
-    
+def process_df(df, log, file_name, number):
     # Tomado de https://stackoverflow.com/questions/66520769/python-contour-polar-plot-from-discrete-data (and more...)
     
     colormap='rainbow'
     values = np.log10(df.measure) if log=='log' else df.measure
-    
-    fig = plt.figure(figsize=(12, 4))
-    
-    ax = fig.add_subplot(121)
-    tcf = ax.tricontourf(df.azimuth, df.altitude, values, levels=256, cmap=colormap)
-    
-    fig.colorbar(tcf)
-    
     theta = np.radians(df.azimuth+90) # For theta=0 at the top
     rad = 90 - df.altitude
-    ax = fig.add_subplot(122)
+    sensor = df["sensor name"].iloc[-1]
+    [day,month,year,_,h0,m0,h1,m1] = file_name.split('-')
+
+    fig, ax = plt.subplots()
     
     ax_polar = fig.add_axes(ax.get_position(), polar=True)
     ax_polar.set_theta_zero_location("N")  # theta=0 at the top
     ax_polar.set_theta_direction(1)  
     ax_polar.set_facecolor('none') # make transparent
-    ax_polar.set_rlim(bottom=df.altitude.max(), top=df.altitude.min())
-    
-    ax.tricontourf(rad * np.cos(theta), rad * np.sin(theta), values, levels=256, cmap=colormap)
+    ax_polar.set_rlim(bottom=df.altitude.max(), top=df.altitude.min())    
+
     ax.set_aspect('equal')
     ax.axis('off')
+
+    tcf = ax.tricontourf(rad * np.cos(theta), rad * np.sin(theta), values, levels=256, cmap=colormap)
+
+    cax = ax.inset_axes([1.15, 0, 0.05, 1])
+    fig.colorbar(tcf, cax=cax)
     
-    #ax.set_title("A line plot on a polar axis", va='bottom')
+    plt.title(f"{sensor}  {day}/{month}/{year}  {h0}:{m0}~{h1}:{m1}")
     
-    plt.show()
+    # plt.show()
+    plt.savefig(f"graphs/{file_name}--{sensor}-{number}.png")
 
 df_total = pd.read_csv(file)
+
 indexAt90 = df_total.loc[df_total.altitude==90].index
 print(indexAt90)
 
+indices = [] # To get rows when altitude changes from max to min (90 to 20, let's say)
 for index, value in df_total.altitude.items():
-    if index > 0:
-        if df_total.loc[index-1].altitude > value:
-            print(index)
-    
-#process_df(df_total, logp)
+    if df_total.iloc[index-1].altitude > value:
+        indices.append(index)
+indices.append(len(df_total))
+print(indices)
+
+file_path = os.path.basename(file)
+file_name = os.path.splitext(file_path)[0]
+
+for i in range(len(indices)-1):
+    df = df_total[indices[i]:indices[i+1]]
+    process_df(df, logp, file_name, i)
